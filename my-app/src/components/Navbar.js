3bdaec25db77222c2320934413a45b9b2c23c36d
Bonnie <bonnieacuna@gmail.com>
2019-03-16 09:43:41 -0700

    updated and created files

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -3,8 +3,8 @@ import React from "react";
 const Navbar = (props) => (
 
   <nav className="navbar navbar-light bg-info">
-    <span className="navbar-brand mb-0 h1">Clicky Game</span>
-    <span className="text-center mb-0 h3">{props.message}</span>
+    <span className="navbar-brand h1">Clicky Game</span>
+    <span className="text-center h3">{props.message}</span>
     <span className="navbar-text text-dark font-weight-bold">
       Score: <span>{props.correct}</span>&nbsp;
       Top Score: <span>{props.topscore}</span>
