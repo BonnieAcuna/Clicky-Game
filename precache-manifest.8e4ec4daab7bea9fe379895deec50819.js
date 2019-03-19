41485090c99b2f8899a52b93df2a7632a290890b
Bonnie <bonnieacuna@gmail.com>
2019-03-18 21:01:24 -0700

    Updates

---
+++ precache-manifest.8e4ec4daab7bea9fe379895deec50819.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "208b49698e1fc680e4659748190a002c",
+    "url": "/Clicky-Game/static/media/vegetarian.208b4969.jpg"
+  },
+  {
+    "revision": "7e37e575fbe01796a332",
+    "url": "/Clicky-Game/static/css/main.9d3688a0.chunk.css"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "6f625d1c5b5808142159",
+    "url": "/Clicky-Game/static/js/2.8e1b0dfd.chunk.js"
+  },
+  {
+    "revision": "54e253eeb04276311b607b6011ff5177",
+    "url": "/Clicky-Game/static/media/pepperoni.54e253ee.jpeg"
+  },
+  {
+    "revision": "c1bf781e4ad73f6bb535ea6a2a81acc5",
+    "url": "/Clicky-Game/static/media/sausage.c1bf781e.jpg"
+  },
+  {
+    "revision": "9752a40309cfe3cfe445eafa67de0f17",
+    "url": "/Clicky-Game/static/media/hawaiian.9752a403.jpg"
+  },
+  {
+    "revision": "1daf3837782992c0d38069bf246eeb35",
+    "url": "/Clicky-Game/static/media/margherita.1daf3837.jpeg"
+  },
+  {
+    "revision": "fb2944186b0472a7c8438d39cbbccd2f",
+    "url": "/Clicky-Game/static/media/supreme.fb294418.jpg"
+  },
+  {
+    "revision": "7e37e575fbe01796a332",
+    "url": "/Clicky-Game/static/js/main.9666dee4.chunk.js"
+  },
+  {
+    "revision": "f1079cc4d35fa8b3522d57e4ec10bec0",
+    "url": "/Clicky-Game/static/media/pesto.f1079cc4.jpeg"
+  },
+  {
+    "revision": "9b4107436cfdbaaa24302060e9c22c62",
+    "url": "/Clicky-Game/static/media/shrimp.9b410743.jpeg"
+  },
+  {
+    "revision": "05c0b9fdb83863ebcf22ac397d57092c",
+    "url": "/Clicky-Game/static/media/bbqchicken.05c0b9fd.jpg"
+  },
+  {
+    "revision": "c569d60e387322f8e859436ab6c3ec4c",
+    "url": "/Clicky-Game/static/media/pickle.c569d60e.jpeg"
+  },
+  {
+    "revision": "c7aa50ba321505aecd1c29ec8f1505fa",
+    "url": "/Clicky-Game/static/media/potato.c7aa50ba.jpg"
+  },
+  {
+    "revision": "9fde44e9e6a11c0b44cf1ce0e819097d",
+    "url": "/Clicky-Game/static/media/cheese.9fde44e9.jpg"
+  },
+  {
+    "revision": "88c6030e59ed5ee57e6bda43235d707c",
+    "url": "/Clicky-Game/static/media/pizzabackground.88c6030e.jpg"
+  },
+  {
+    "revision": "5e9887eec0d04d831e46522b30ebe519",
+    "url": "/Clicky-Game/index.html"
+  }
+];
